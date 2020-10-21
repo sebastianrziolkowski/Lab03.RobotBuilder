4a9a073c04efe67dcb1d857103932012f89561b2
Sebastian Ziolkowski <osomjokey2@gmail.com>
2020-10-21 18:03:10 +0200

    init commit

---
+++ RobotBuilder/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Robot robot = new Robot("BB",8);
+            robot.DajSygnal();
+            Console.WriteLine(robot.Seria);
+            robot.Uruchom();
+            robot.DajSygnal();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RobotBuilder/Robot.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotBuilder
+{
+    class Robot
+    {
+
+        private string _kodSerii;
+        private int _nrSerii;
+        private bool _wlaczony;
+
+        public Robot(string kodSerii, int nrSerii)
+        {
+            this._kodSerii = kodSerii;
+            this._nrSerii = nrSerii;
+            this._wlaczony = false;
+        }
+
+
+        public string Seria => _kodSerii + "-" + _nrSerii;
+
+        public void Uruchom()
+        {
+            if(_wlaczony)
+                    Console.WriteLine("Robot already running!");
+            else
+            {
+                _wlaczony = true;
+                Console.WriteLine("Robot turning on");
+            }
+
+        }
+
+        public void DajSygnal()
+        {
+            if(_wlaczony)
+                Console.WriteLine("Signal: Wrrrp bleep");
+            else
+                Console.WriteLine("...");
+        }
+
+        public override string ToString()
+        {
+            return "Robot";
+        }
+    }
+}
